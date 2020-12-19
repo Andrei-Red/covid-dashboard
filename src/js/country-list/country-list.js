4a90f8d55935db277bf5357db3f30dbf95f429e7
Kirill Stakheyko <kirill.stakheyko@gmail.com>
2020-12-20 02:19:14 +0300

    feat: search by country

--- src/js/country-list/country-list.js
+++ src/js/country-list/country-list.js
@@ -1,52 +1,77 @@
 export default class List {
     constructor() {
         this.list = document.querySelector('.list-content');
-        this.url = 'https://api.covid19api.com/summary';
-        this.urlFlag = 'https://restcountries.eu/rest/v2/all?fields=name;population;flag';
+        this.url = 'https://corona.lmao.ninja/v2/countries';
         this.data = null;
+        this.dataToShow = null;
+        this.controlPanel = null;
+        this.dataPanel = null;
     }
 
     loadInfo() {
-        fetch('./js/country-list/covid.json')
+        fetch(this.url)
             .then((response) => response.json())
-            // eslint-disable-next-line no-return-assign
-            .then((json) => this.data = json.Countries);
-        this.loadFlags();
+            .then((json) => this.saveInfo(json))
     }
 
-    loadFlags() {
-        fetch(this.urlFlag)
-            .then((response) => response.json())
-            .then((json) => this.showInfo(json));
+    saveInfo(json) {
+        this.data = json;
+        this.dataToShow = this.data;
+        this.sortInfo('cases');
+        this.renderListPanel();
+        this.showInfo();
     }
 
-    showInfo(flags) {
-        const dataToShow = this.data.sort((a, b) => a.TotalConfirmed - b.TotalConfirmed).reverse();
-        dataToShow.forEach(data => {
+    sortInfo(param) {
+        this.dataToShow = this.dataToShow.sort((a, b) => a[param] > b[param] ? 1 : -1).reverse();
+    }
+
+    searchInfo(request) {
+        this.dataToShow = this.data.filter((country) => country
+            .country.substr(0, request.length).toLowerCase() === request);
+    }
+
+    renderListPanel() {
+        this.list.innerHTML = '';
+        this.controlPanel = document.createElement('div');
+        this.controlPanel.classList.add('control-panel');
+        this.dataPanel = document.createElement('div');
+        const searchField = document.createElement('input');
+        searchField.type = 'text';
+        searchField.placeholder = 'input country'
+        this.controlPanel.appendChild(searchField);
+        searchField.oninput = () => {
+            this.searchInfo(searchField.value);
+            this.sortInfo();
+            this.showInfo();
+        };
+
+        this.list.append(this.controlPanel, this.dataPanel);
+    }
+
+
+    showInfo() {
+        this.dataPanel.innerHTML = '';
+        this.dataToShow.forEach(data => {
             const listItem = document.createElement('div');
             listItem.classList.add('list-item');
 
             const number = document.createElement('span');
             number.classList.add('number');
-            number.innerText = data.TotalConfirmed;
+            number.innerText = data.cases;
 
             const country = document.createElement('span');
             country.classList.add('country');
-            country.innerText = data.Country;
+            country.innerText = data.country;
             const imageWrapper = document.createElement('div');
-            const flag = flags.find((item) => item.name === data.Country);
-            if (flag) {
-                const image = document.createElement('img');
 
-                imageWrapper.classList.add('country-flag');
-                image.onload = () => imageWrapper.appendChild(image);
-                image.src = flag.flag;
-            }
+            const image = document.createElement('img');
 
-            listItem.appendChild(number);
-            listItem.appendChild(country);
-            listItem.appendChild(imageWrapper)
-            this.list.appendChild(listItem);
+            imageWrapper.classList.add('country-flag');
+            image.onload = () => imageWrapper.appendChild(image);
+            image.src = data.countryInfo.flag;
+            listItem.append(number, country, imageWrapper);
+            this.dataPanel.appendChild(listItem);
         });
 
     }
