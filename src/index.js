e46277711ddf6686e99d7d0f318415ee856ae546
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-12-23 23:52:13 +0300

    feat: add main features of the table

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import startCreteHTML from './js/create-main-html.js';
 import createMapInApp from './js/map/map-main.js';
+import createTableInApp from './js/table/table.js';
 import List from './js/country-list/country-list.js';
 
 // RUN
 startCreteHTML();
 createMapInApp();
+createTableInApp();
 const list = new List();
 list.loadInfo();
 
